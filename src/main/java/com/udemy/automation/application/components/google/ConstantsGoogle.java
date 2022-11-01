9bf51b2ecf8a66ca6c5a62a1d676556d674c0f7e
Enrique Oreiro <dsmountreax@gmail.com>
2022-11-01 11:15:29 -0500

    google application

---
+++ src/main/java/com/udemy/automation/application/components/google/ConstantsGoogle.java
@@ -0,0 +1,15 @@
+package com.udemy.automation.application.components.google;
+
+public class ConstantsGoogle {
+
+    public static final String LOGO_GOOGLE=".lnXdpd"; // nuevo locator de Logo Google
+    public static final String BUSCAR_GOOGLE="(//input[@name='btnK'])[2]"; // Locator nuevo Xpath
+    public static final String SIENTO_CON_SUERTE="(//input[@name='btnI'])[2]"; // locator nuevo
+    public static final String BUSCAR="[name='q']"; // Nuevo Locator
+
+    //constantes para assertions
+    public static final String BUSCAR_GOOGLE_TITLE="Buscar con Google";
+    public static final String SIENTO_CON_SUERTE_TITLE="Me siento con suerte ";
+
+
+}
